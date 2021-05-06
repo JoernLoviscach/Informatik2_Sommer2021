b2cd83f870a15a349de032cb141615223ce5d673
Jörn Loviscach <jl@j3L7h.de>
2021-05-06 16:41:13 +0200

    Nach Veranstaltung 2021-05-06

---
+++ Fuhrpark/Fuhrpark/Fuhrpark.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Fuhrpark
+{
+    class Fahrzeug // später kommt: abstract
+    {
+        static int anzahlFahrzeuge;
+
+        public Fahrzeug()
+        {
+            anzahlFahrzeuge++;
+        }
+
+        public virtual double SchätzeReichweite() // später kommt: abstract
+        {
+            return double.NaN;
+        }
+
+        public virtual void FahreStrecke(double entfernung) // später kommt: abstract
+        {
+        }
+    }
+
+    class Lkw : Fahrzeug
+    {
+        string kennzeichen;
+    }
+
+    class Pkw : Fahrzeug
+    {
+        string kennzeichen;
+        public Pkw(string kennzeichen)
+        {
+            this.kennzeichen = kennzeichen;
+        }
+    }
+
+    class PkwBenzin : Pkw
+    {
+        double tankfüllstand;
+        public PkwBenzin(string kennzeichen)
+            : base(kennzeichen)
+        {
+        }
+    }
+
+    class PkwElektro : Pkw
+    {
+        double stateOfChargeProzent;
+        double stateOfHealthProzent;
+        double maximaleReichweite;
+        public PkwElektro(string kennzeichen, double maximaleReichweite)
+            : base(kennzeichen)
+        {
+            this.maximaleReichweite = maximaleReichweite;
+            stateOfHealthProzent = 100.0;
+            stateOfChargeProzent = 100.0;
+        }
+        public void Lade(double ladung_kwh)
+        {
+            stateOfChargeProzent = Math.Min(100.0, stateOfChargeProzent + 100.0 * ladung_kwh / 50.0);
+        }
+        public override void FahreStrecke(double entfernung)
+        {
+            stateOfHealthProzent = stateOfHealthProzent * Math.Exp(-entfernung / 50000.0);
+            // Schätzwert!!
+            stateOfChargeProzent = Math.Max(0.0, stateOfChargeProzent - 100.0 * entfernung / (maximaleReichweite * stateOfHealthProzent / 100.0));
+        }
+        public override double SchätzeReichweite()
+        {
+            return maximaleReichweite * stateOfChargeProzent / 100.0 * stateOfHealthProzent / 100.0;
+        }
+    }
+
+    class HerkömmlichesFahrrad : Fahrzeug
+    {
+        public override double SchätzeReichweite()
+        {
+            return 10.0;
+        }
+    }
+
+    class Ladestation
+    {
+    }
+}
---
+++ Fuhrpark/Fuhrpark/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fuhrpark
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /*  Drei Säulen der objektorientierten Programmierung:
+         *  - Kapselung ("Encapsulation")
+         *       - Daten und Funktionen zusammen
+         *       - privat/öffentlich
+         *  - Vererbung ("Inheritance")
+         *       - Kindklassen
+         *       - Vererbung der Implementation
+         *       - Vererbung der Schnittstelle ("Interface"),
+         *            siehe: virtuelle Methoden (vgl. Polymorphie)
+         *  - Polymorphie ("Polymorphism")
+         */
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            PkwElektro auto1 = new PkwElektro("BI XY 123", 300.0);
+            auto1.FahreStrecke(50.0);
+            double r1 = auto1.SchätzeReichweite();
+            auto1.FahreStrecke(100.0);
+            r1 = auto1.SchätzeReichweite();
+            auto1.Lade(20.0);
+            r1 = auto1.SchätzeReichweite();
+
+            Fahrzeug[] fuhrpark = new Fahrzeug[3];
+            fuhrpark[0] = auto1;
+            fuhrpark[1] = new HerkömmlichesFahrrad();
+            fuhrpark[2] = new Lkw();
+            for (int i = 0; i < fuhrpark.Length; i++)
+            {
+                fuhrpark[i].FahreStrecke(10.0);
+            }
+        }
+    }
+}
